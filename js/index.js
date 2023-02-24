97780781b1e3949193335eb05c13159000ab73ea
Malk0v <106482398+Malk0v@users.noreply.github.com>
2023-02-24 16:48:52 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -12,12 +12,18 @@ refs.reset.addEventListener("click", remove);
 
 function add() {
   a += 1;
-  if (a < 5) {
+  if (a < 10) {
     refs.test.innerHTML = `Молодец что нажал ${a} раз `;
     return
   }
-  refs.test.innerHTML = `ОГО! Ото у тебя терпение, молодец что нажал ${a} раз `;
+
+  if (a < 20) {
+    refs.test.innerHTML = ` Это уже не смешно, что ты нажал ${a} раз `;
+    return
+  }
+  refs.test.innerHTML = `ОГО! Ото у тебя терпение, не каждый может нажать ${a} раз `;
   console.log("hello");
+  ad
 }
 
 function remove() {
