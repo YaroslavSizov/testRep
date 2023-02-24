865685ba69492a6bfba764375b37093f5648f49b
Malk0v <106482398+Malk0v@users.noreply.github.com>
2023-02-24 16:59:37 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,31 +2,46 @@ const refs = {
   test: document.querySelector(".js-test"),
   btn: document.querySelector(".js-btn"),
   reset: document.querySelector(".js-reset"),
+  body: document.querySelector(".body"),
 };
 
 let a = 0;
-
 refs.btn.addEventListener("click", add);
-
 refs.reset.addEventListener("click", remove);
 
 function add() {
   a += 1;
   if (a < 10) {
-    refs.test.innerHTML = `Молодец что нажал ${a} раз `;
-    return
+    first();
+      return;
   }
 
   if (a < 20) {
-    refs.test.innerHTML = ` Это уже не смешно, что ты нажал ${a} раз `;
+    second();
     return
   }
-  refs.test.innerHTML = `ОГО! Ото у тебя терпение, не каждый может нажать ${a} раз `;
-  console.log("hello");
-  ad
+
+  third();
 }
 
 function remove() {
   refs.test.innerHTML = " ";
   console.log("good bay)");
+}
+
+function first() {
+refs.test.innerHTML = `Молодец что нажал ${a} раз `;
+}
+
+function second() {
+refs.btn.textContent = "может хватит уже??";
+refs.test.innerHTML = ` Это уже не смешно, что ты нажал ${a} раз `;
+refs.body.style.fontSize = "24px";
+}
+
+function third() {
+refs.test.innerHTML = `ОГО! Ото у тебя терпение, не каждый может нажать ${a} раз `;
+console.log("hello");
+refs.body.style.backgroundColor = "red";
+refs.body.style.fontSize = "34px";
 }
